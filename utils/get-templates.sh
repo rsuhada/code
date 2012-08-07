a826cef917f38ec3d3c8e2dc6b82e57e764bf0d0
rsuhada <suhada.robert@gmail.com>
2012-08-07 16:47:41 +0200

    updated fixed sed mac/linux compatibility issue

--- utils/get-templates.sh
+++ utils/get-templates.sh
@@ -21,8 +21,8 @@ echo ${name} ${obsid}
 
 # conf file
 cp ~/data1/sw/esaspi/templates/template.conf configs/${name}.conf
-sed -i .bk "s/1XXXXXXXXXX/${obsid}/g" configs/${name}.conf ; rm configs/${name}.conf
-sed -i .bk "s/2XXXXXXXXXXXXXXXXX/${name}/g" configs/${name}.conf ; rm configs/${name}.conf
+sed -i.bk "s/1XXXXXXXXXX/${obsid}/g" configs/${name}.conf ; rm configs/${name}.conf.bk
+sed -i.bk "s/2XXXXXXXXXXXXXXXXX/${name}/g" configs/${name}.conf ; rm configs/${name}.conf.bk
 
 # modules file
 cp ~/data1/sw/esaspi/templates/template.modules configs/${name}.modules
