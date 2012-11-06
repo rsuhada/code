6445d8d58bea357d42b69d9af7e97040df7caac8
rsuhada <suhada.robert@gmail.com>
2012-11-06 12:12:52 +0100

    found the problem - it was in shift function using interpolation

--- py/test/test_2d_im_lmfit.py
+++ py/test/test_2d_im_lmfit.py
@@ -725,6 +725,10 @@ def test_lmfit_v06_psf_1d(fname='cluster-im-v06-psf.fits'):
     shift(distmatrix_trim, (1,1), output=distmatrix_trim)
     distmatrix_trim = trim_fftconvolve(distmatrix_trim)
 
+    distmatrix_trim2 = distmatrix.copy()
+    distmatrix_trim2 = roll(roll(distmatrix_trim2,1,axis=0),1,axis=1)
+    distmatrix_trim2 = trim_fftconvolve(distmatrix_trim2)
+
     # print model.max(), model.shape, where(model==model.max()), where(distmatrix_trim==distmatrix_trim.min()), distmatrix_trim.min()
 
     # data
@@ -732,29 +736,89 @@ def test_lmfit_v06_psf_1d(fname='cluster-im-v06-psf.fits'):
     (profile_data, geometric_area_data) = extract_profile_fast2(data, distmatrix, bgrid)
     profile_norm_data = profile_data[0:r_length] / geometric_area_data[0:r_length]    # trim the corners
 
-    model = data.copy()
-
-    # Mon Nov  5 17:18:58 2012
-    # start here! some problem
-
-    print sum(model)
-    shift(model, (1,1), output=model)
-    model = trim_fftconvolve(model)
-    print sum(model)
-
     # model
     print 'model:', model.shape, distmatrix_trim.shape, distmatrix_trim.min(), bgrid.max()
     (profile_model, geometric_area_model) = extract_profile_fast2(model, distmatrix_trim, bgrid)
     profile_norm_model = profile_model[0:r_length] / geometric_area_model[0:r_length]    # trim the corners
 
-    hdu = pyfits.PrimaryHDU(model, hdr)    # extension - array, header
+    hdu = pyfits.PrimaryHDU(distmatrix, hdr)    # extension - array, header
     hdulist = pyfits.HDUList([hdu])                  # list all extensions here
-    hdulist.writeto('d.fits', clobber=True)
+    hdulist.writeto('d1.fits', clobber=True)
+
+    hdu = pyfits.PrimaryHDU(distmatrix_trim, hdr)    # extension - array, header
+    hdulist = pyfits.HDUList([hdu])                  # list all extensions here
+    hdulist.writeto('d2.fits', clobber=True)
+
+    hdu = pyfits.PrimaryHDU(distmatrix_trim2, hdr)    # extension - array, header
+    hdulist = pyfits.HDUList([hdu])                  # list all extensions here
+    hdulist.writeto('d3.fits', clobber=True)
+
+    # print
+    # print r_data[idx], profile_norm_data[idx], geometric_area_data[idx]
+    # print r_true[idx], profile_norm_model[idx], geometric_area_model[idx]
+    # print
+
+    # # bgrid[1] = 1.99
+    # print
+    # print 'MODEL'
+    # print bgrid[0], bgrid[1]
+    # idx = where(logical_and((distmatrix_trim>=bgrid[0]), (distmatrix_trim<bgrid[1])))
+    # idx = where(distmatrix_trim<2.0)
+    # print distmatrix_trim.min(), 'min'
+    # print zip(idx[0]+1, idx[1]+1)
+    # print model[idx]
+    # print distmatrix_trim[idx]
 
 
-    print r_data[idx], profile_norm_data[idx], geometric_area_data[idx]
-    print r_true[idx], profile_norm_model[idx], geometric_area_model[idx]
     print
+    print
+    print
+    print 70*'#'
+    print
+
+    a =  distmatrix_trim2[44:47,44:47]
+    print a
+    print
+
+    # idx = where(a < 2.0)
+    # print zip(idx[0], idx[1])
+
+    print a<2
+    print a[1,0], a[1,0]<2
+    print a[1,2], a[1,2]<2
+    print ("%20.20f" % a[1,0])
+    print ("%20.20f" % a[1,2])
+
+    # print
+    # print 'DATA'
+    # print bgrid[0], bgrid[1]
+    # idx = where(logical_and((distmatrix>=bgrid[0]), (distmatrix<bgrid[1])))
+    # print zip(idx[0]+1, idx[1]+1)
+    # print data[idx]
+    # print distmatrix[idx]
+
+    # hdu = pyfits.PrimaryHDU(model, hdr)    # extension - array, header
+    # hdulist = pyfits.HDUList([hdu])                  # list all extensions here
+    # hdulist.writeto('model.fits', clobber=True)
+
+    # print
+    # print 70*'#'
+    # print
+    # info(distmatrix_trim)
+    # info(a)
+    # print
+    # print 70*'#'
+    # print
+
+    # print
+    # print 70*'#'
+    # print
+    # info(distmatrix)
+    # print
+    # print 70*'#'
+    # print
+
+
 
 # in: 246
 # model: 250
