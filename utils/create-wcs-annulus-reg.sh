c1b871c8850496278b8795d506f1b3eddc2239f4
rsuhada <suhada.robert@gmail.com>
2013-01-22 16:23:18 +0100

    updates

---
+++ utils/create-wcs-annulus-reg.sh
@@ -0,0 +1,15 @@
+# simple script to create a circle region for analysis
+
+id=$1
+ra=$2
+de=$3
+
+echo "
+
+# Region file format: DS9 version 4.1
+# Filename: dummy.fits
+global color=green font=\"helvetica 10 normal\" select=1 highlite=1 edit=1 move=1 delete=1 include=1 fixed=0 source=1
+fk5
+annulus($ra, $de, 150\", 300\") # color=green width=2 font=\"helvetica 24 normal\" text={$id}
+
+"
