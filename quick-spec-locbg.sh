7af23e0893a2ffe42a2ef16f28fc17b4a5c8c4e3
rsuhada <suhada.robert@gmail.com>
2013-02-05 14:57:20 +0100

    hc removal in iter/q/radspec

--- quick-spec-locbg.sh
+++ quick-spec-locbg.sh
@@ -11,35 +11,6 @@ specdir=../spec
 
 source ${codedir}/utils/util-funcs-lib.sh
 
-# # FIXME: available in a standalone script - this function is redundant
-# function get_cluster_pars {
-
-#     ######################################################################
-#     # looks through an analysis notebook file returning the requested
-#     # parameters. Input is an array, e.g.  pars=(RA DE RA60
-#     # DE60 X_IM Y_IM X_PHY Y_PHY NH REDSHIFT), output is a string
-
-#     if [[ -e $NOTESFILE ]]
-#     then
-
-#         pars=$1
-#         declare -a values
-
-#         for i in ${!pars[@]}
-#         do
-#             val=`grep ${pars[i]} $NOTESFILE | head -1 | awk '{print $2}'`
-#             values=( "${values[@]}" "$val" )
-#             # echo ${pars[i]} ${values[i]}
-#         done
-
-#         # this is the output
-#         echo ${values[@]}
-
-#     else
-#         echo "$NOTESFILE does not exist, not returning parameters!"
-#     fi
-# }
-
 ######################################################################
 # settings
 
@@ -153,7 +124,6 @@ echo "PN CCD pattern: " $pnccdpattern
 # esas
 mospattern="(FLAG == 0) && (PATTERN<=12) && (PI in [100:10000])"
 pnpattern="(FLAG == 0) && (PATTERN<=4) && (PI in [100:10000])"
-
 
 if [[ $EXTRACT_SRC -ne 0 ]]
 then
