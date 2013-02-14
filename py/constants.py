df7f04ce470f142612e7a54b3c6699bc60f1f0bc
rsuhada <suhada.robert@gmail.com>
2013-02-14 14:33:20 +0100

    started adding density normalization calculation

---
+++ py/constants.py
@@ -0,0 +1,28 @@
+######################################################################
+# physical constatnts
+cc_cgs = 2.99792458e10                        # Speed of light [cgs]
+bk_cgs = 1.3806503e-16                        # Boltzmann's constant [cgs]
+hp_cgs = 6.62606876e-27                       # Planck's constant [cgs]
+
+
+# mass
+mp_cgs = 1.67262158e-24                   # Proton rest mass [cgs]
+me_cgs = 9.10938188e-28                       # Electron rest mass [cgs]
+
+
+######################################################################
+# astrophysical
+
+# mass
+msun_cgs = 1.989e33                           # Solar Mass [cgs]
+
+# distance
+mpc_to_cm = 3.085677582e24                # megaparsec [cgs]
+kpc_to_cm = 3.085677582e21                # megaparsec [cgs]
+
+######################################################################
+# composition
+
+# relative molecular weights: Feldman 1992, A=0.3
+mu_e_feldman92 = 1.167
+mu_h_feldman92 = 1.400
---
+++ py/xspec_utils.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+from numpy import *
+from cosmo_dist import dist_ang
+from constants import *
+
+
+def spec_norm_to_density(norm, z, da, r_proj_ang, r_proj_inner_ang=0.0):
+    """
+    Convert XSPEC normalization to density for a spherical region,
+    optionally with excised inner sphere.
+
+    Arguments:
+    - `norm`: XSPEC normalization in XSPEC units
+    - `z`: redsihift
+    - `da`: angular diameter distance [Mpc]
+    - `r_proj_ang`: projected radius of the fitted spherical region
+      [arcsec]
+    - `r_proj_inner_ang`: [Optional] projected radius of the inner
+      excised spherical region [arcsec]
+
+    Output:
+    - `density`: density in cgs [g cm**-3]
+    """
+
+    # relative molecular weights: Feldman 1992, A=0.3
+    mu_e = mu_e_feldman92
+    mu_h = mu_h_feldman92
+
+    # convert everything to cgs
+    da = da * mpc_to_cm
+    r_proj = r_proj_ang * da
+    r_proj_inner = r_proj_inner_ang * da
+
+    b = r_proj**3 - r_proj_inner**3
+
+    if b>0.0:
+        density = (da*(1+z)) * mp_cgs * sqrt(3.0e14 * mu_e * mu_h * norm / b)
+    else:
+        density = 0.0
+        print "*** Error: Source region has zero volume!"
+
+    return density
+
+
+if __name__ == '__main__':
+    ######################################################################
+    # test normalization calculation
+
+    # cosmology
+    h_0=70.2
+    omega_m_0=0.272
+    omega_de_0=0.728
+    omega_k_0=0.0
+
+    # cluster
+    z = 0.468
+    r_proj_ang = 60.0   # projected radius [arcsec]
+
+    norm = 1.45738E-03
+    norm_err_n = -4.5767e-05
+    norm_err_p = +4.53208e-05
+
+    # angular distance
+    da = dist_ang(z=z, h_0=h_0, omega_m_0=omega_m_0, omega_de_0=omega_de_0, omega_k_0=omega_k_0) # [Mpc]
+
+    density = spec_norm_to_density(norm, z, da, r_proj_ang)
+
+    ne = density / (mu_e_feldman92 * mp_cgs)
+
+    print
+    print " z          :: ", z
+    print " da         :: ", da
+    print " r_proj_ang :: ", r_proj_ang
+    print " r_proj     :: ", r_proj
+    print " norm       :: ", norm
+    print " density    :: ", density
+    print " ne density :: ", ne
+
+
+
+
+
